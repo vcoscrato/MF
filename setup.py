4d86cf35dd363ef57c273ac0a15cd0dab5a95e3f
Victor Coscrato <vcoscrato@gmail.com>
2024-08-14 19:21:33 +0100

    fix issue #3`

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='uncertain',
     version='0.4.0',
     packages=find_packages(),
-    install_requires=['torch>=1.7.0', 'numpy', 'scipy', 'pytorch-lightning', 'h5py', 'pandas', 'requests', 'matplotlib',
+    install_requires=['torch>=1.7.0', 'numpy', 'scipy', 'pytorch-lightning', 'h5py', 'pandas>=1.0.0, <=2.0.0', 'requests', 'matplotlib',
                       'scikit-learn', 'tqdm'],
     license='MIT',
     classifiers=['Development Status :: 1 - Beta',
